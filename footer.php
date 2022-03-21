834ac74180576b8de311cebb9ba5f8b4b31a3020
Amias-Burrows <amias.burrows@gmail.com>
2022-03-21 22:05:16 +0000

    Add theme files and started with header

---
+++ footer.php
@@ -0,0 +1,40 @@
+		<footer class='secondary'>
+			<ul>
+				<li>
+					<a href='/res/privacy.pdf'>Privacy Policy</a>
+				</li>
+				<li>
+					<a href='/res/terms.pdf'>Terms & Conditions</a>
+				</li>
+				<li>
+					<a href='/res/use.pdf'>Acceptable Use Policy</a>
+				</li>
+				<li>
+					<a href='/res/cookie.pdf'>Cookie Policy</a>
+				</li>
+				<li>
+					<a href='https://www.fsb.org.uk'>FSB Member</a>
+				</li>
+			</ul>
+			<p>Copyright &copy; AR Burrows Web Development 2020 - Present</p>
+		</footer>
+
+		<!-- Calendly badge widget begin -->
+		<link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+		<script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+		<script type="text/javascript">
+			window.onload = function() {
+				Calendly.initBadgeWidget({
+					url: 'https://calendly.com/ar-burrows-web-development/30min',
+					text: 'Schedule time with me',
+					color: '#0069ff',
+					textColor: '#ffffff',
+					branding: true
+				});
+			}
+		</script>
+		<!-- Calendly badge widget end -->
+		<script src="/res/js/main.js"></script>
+		<script src='/res/js/cookie.js'></script>
+	</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+
+<main>
+
+	<?php
+	get_post();
+	?>
+
+</main>
+
+<?php
+get_footer();
+?>
