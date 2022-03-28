132e9d29077a16793ca8a541e2fb60935038c4c6
amias-burrows <amias.burrows@gmail.com>
2022-03-28 21:38:52 +0100

    Add new headers
    I need to work on a html-only version just to get the right css files.
    Also I've decided I don't like how my css is set out

---
+++ header-categories.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta property="og:site_name" content="<?php the_title(); ?>"/>
+		<meta property='og:type' content='website'/>
+		<meta property='og:locale' content='en_GB'/>
+		<meta property="og:url" content="<?php echo site_url(); ?>"/>
+		<?php
+		$post = get_post();
+		$thumbnail = get_the_post_thumbnail_url($post->ID);
+		if ($thumbnail == false) {
+			$id = 'https://picsum.photos/1024/600?blur=3';
+		} else {
+			$id = $thumbnail;
+		}
+		?>
+		<meta property='og:image' content='<?php echo $id; ?>'/>
+		<meta property="og:title" content="<?php the_title(); ?>"/>
+		<?php
+		if (get_the_excerpt() != null) {
+			$excerpt = get_the_excerpt();
+		} else {
+			$excerpt = get_theme_mod('amias_open_graph_excerpt');
+		}
+		?>
+		<meta property="og:description" content="<?php echo $excerpt; ?>"/>
+
+		<?php
+		wp_head();
+		?>
+
+			<!-- Google Tag Manager
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZPE0E8RNM4"></script><script src="/res/js/analytics.js"></script>
+			-->
+	</head>
+	<body>
+		<header>
+			<div id='content'>
+				<?php
+				if (function_exists('the_custom_logo')) {
+					$custom_logo_id = get_theme_mod('custom_logo');
+					$logo = wp_get_attachment_image_src($custom_logo_id);
+				}
+				?>
+				<img id='logo' src='<?php echo $logo[0]; ?>' alt='AR Burrows Web Development Logo'/>
+					<div id='contact'>
+						<div id='handle'>
+							<?php
+							include get_template_directory() . '/inc/svg/ellipsis.php';
+							?>
+						</div>
+						<a id='phone' href='tel:<?php echo get_theme_mod('amias_contact_phone'); ?>'>
+							<?php
+							include get_template_directory() . '/inc/svg/phone.php';
+							?>
+						</a>
+						<a id='email' href='mailto:<?php echo get_theme_mod('amias_contact_email'); ?>'>
+							<?php
+							include get_template_directory() . '/inc/svg/email.php';
+							?>
+						</a>
+						<a id='download' href='<?php echo get_theme_mod('amias_contact_contact'); ?>'>
+							<?php
+							include get_template_directory() . '/inc/svg/contact.php';
+							?>
+						</a>
+					</div>
+					<?php
+					include get_template_directory() . '/inc/svg/hamburger.php';
+					?>
+					<nav class='secondary'>
+						<?php
+						wp_nav_menu(
+							array(
+								'menu' => 'main',
+								'menu_id' => 'main',
+								'container' => 'ul',
+								'theme_location' => 'main'
+							)
+						);
+						?>
+						<?php
+						wp_nav_menu(
+							array(
+								'menu' => 'social',
+								'menu_id' => 'socials',
+								'container' => 'ul',
+								'theme_location' => 'socials'
+							)
+						);
+						?>
+					</nav>
+				</div>
+			</div>
+		</header>
