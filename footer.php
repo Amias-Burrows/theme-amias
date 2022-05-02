56a38010320b9223aea799a885af582a81bcebcc
amias-burrows <amias.burrows@gmail.com>
2022-05-02 15:33:33 +0100

    Add extra elements in
    Definitely put them somewhere.  Just no idea where that is

--- footer.php
+++ footer.php
@@ -11,6 +11,9 @@
 			<p>Copyright &copy; AR Burrows Web Development 2020 - Present</p>
 		</footer>
 		<?php
+		include get_template_directory_uri() . '/inc/contact-widget.php';
+		include get_template_directory_uri() . '/inc/dark-mode-toggle.php';
+		include get_template_directory_uri() . '/inc/scroll-to-content.php';
 		wp_footer();
 		?>
 	</body>
