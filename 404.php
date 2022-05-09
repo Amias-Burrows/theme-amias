368b5b35849b0d3a92ce52787cf8de6e10296814
amias-burrows <amias.burrows@gmail.com>
2022-05-09 16:12:10 +0100

    Modifying 404 page.
    forgot to do this but by tomorrow it will all be finished.  That is my
    final date to edit the website.  I've got the morning to finish and then
    it's going live.  Gonna get some content written up for it later

--- 404.php
+++ 404.php
@@ -1,9 +1,21 @@
 <?php
-get_header();
+get_header('search');
 ?>
 <main>
-	<h2>Oh No!</h2>
-	<h3>Did I send you to the wrong place</h3>
+	<img class='background' src='https://picsum.photos/1024/600'/>
+	<h1>Oh No!</h1>
+	<h2>I must have mistyped.  If you think this page should exist let me know via the contact form below.</h2>
+	<form>
+		<input type='text' value='<?php echo the_permalink(); ?>' placeholder='Type incorrect URL here'/>
+		<label for='message'>Type any additional information here</label>
+		<p>e.g.</p>
+		<ul>
+			<li>Where you found the link</li>
+			<li>What you were trying to find</li>
+		</ul>
+		<input type='text' name='message'/>
+		<input type='submit' value='Submit Error Report'/>
+	</form>
 </main>
 <?php
 get_footer();
